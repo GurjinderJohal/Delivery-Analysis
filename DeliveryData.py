ade86c8e5232be03d509ce5c9f805e5d7917f59a
GurjinderJohal <gurjinderj@gmail.com>
2025-06-25 15:42:50 -0700

    Update DeliveryData.py
    Added Heat Map for deliveries of specified weeks. Also added function for getting latitude and longitude of addresses

--- DeliveryData.py
+++ DeliveryData.py
@@ -18,6 +18,54 @@ def getWeeklyDeliveries():
     df['Week'] = df['Date'].dt.to_period('W-SAT')
     weekly_counts = df.groupby('Week').size().reset_index(name='Total Deliveries')
     print(weekly_counts)
+
+#generate a heat map for deliveries for specified week ex. 2025-04-28
+def generateHeatMap(start_of_week):
+
+    df = df_2025
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    start_date = pd.to_datetime(start_of_week)
+    end_date = start_date + timedelta(days=6)
+
+    df_week = df[(df['Date'] >= start_date) & (df['Date'] <= end_date) ]
+
+    addresses = list(df_week['Address'])
+
+    #get latitude and longitude of addresses
+    locations = get_locations(addresses)
+
+    df_locations = pd.DataFrame(locations)
+    df_locations = df_locations.rename(columns={0:'Address',1:'latitude',2:'longitude'})
+    
+    #merge the latitude and longitude
+    df_week = df_week.merge(df_locations,on='Address',how='left')
+    df_week = df_week.dropna(subset=['latitude', 'longitude'])
+
+    map_center = [df_week['latitude'].mean(),df_week['longitude'].mean()]
+
+    m = folium.Map(location=map_center, zoom_start=12)
+
+    #generate heatmap
+    heat_data = df_week[['latitude','longitude']]
+    HeatMap(heat_data).add_to(m)
+
+    m.save("delivery_heatmap_week.html")
+    print("Heatmap saved")
+
+#get latitude and longitude for addresses
+def get_locations(addresses):
+    geolocator = Nominatim(user_agent="address_mapper")
+    locations = []
+    for address in addresses:
+        try:
+            location = geolocator.geocode(address)
+            if location:
+                locations.append((address, location.latitude, location.longitude))
+        except Exception as e:
+            print(f"Error geocdoing {address}: {e}")
+        time.sleep(2)
+    return locations
     
 #get inactive customers that have not ordered in the last 6 months
 def getInactiveCustomers():
@@ -67,17 +115,9 @@ def mapInactiveCustomers():
     getInactiveCustomers()
     inactive_customers = pd.read_excel('inactive_customers.xlsx')
     addresses = list(inactive_customers['Address'])
-    geolocator = Nominatim(user_agent="address_mapper")
 
-    locations = []
-    for address in addresses:
-        try:
-            location = geolocator.geocode(address)
-            if location:
-                locations.append((address, location.latitude, location.longitude))
-        except Exception as e:
-            print(f"Error geocdoing {address}: {e}")
-        time.sleep(2)
+    #grab latitude and longitude of addresses
+    locations = get_locations(addresses)
 
     if locations:
         avg_lat = sum(lat for _, lat, _ in locations) / len(locations)
